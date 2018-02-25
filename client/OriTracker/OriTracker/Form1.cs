f22aee28ba78533fbbba49a132d959c37f82fb17
Xavier Shay <contact@xaviershay.com>
2018-02-24 16:32:30 -0800

    PoC client

---
+++ client/OriTracker/OriTracker/Form1.cs
@@ -0,0 +1,92 @@
+﻿using Google.Cloud.Firestore;
+using LiveSplit.OriDE.Memory;
+using Nemiro.OAuth.LoginForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OriTracker
+{
+    [FirestoreData]
+    public class TraceEvent
+    {
+        [FirestoreProperty]
+        public float X { get; set; }
+
+        [FirestoreProperty]
+        public float Y { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+		private OriMemory Memory { get; set; }
+        private FirestoreDb db { get; set; }
+        private PointF? lastPos;
+
+        static HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // TODO: Do this off main thread, at very least does FS access. Maybe does network too?
+            //db = FirestoreDb.Create("ori-tracker");
+
+            client.BaseAddress = new Uri("https://us-central1-ori-tracker.cloudfunctions.net/");
+
+            Memory = new OriMemory();
+			Thread t = new Thread(UpdateLoop);
+			t.IsBackground = true;
+			t.Start();
+        }
+
+        private void UpdateLoop() {
+            bool lastHooked = false;
+            while (true) {
+                bool hooked = false;
+                try {
+                    hooked = Memory.HookProcess();
+                } catch { }
+
+                if (hooked) {
+                    UpdateValues();
+                }
+                if (lastHooked != hooked) {
+                    lastHooked = hooked;
+                }
+                Thread.Sleep(200);
+            }
+        }
+        public void UpdateValues()
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            var now = t.TotalMilliseconds;
+            PointF pos = Memory.GetCameraTargetPosition();
+            if (!lastPos.HasValue || lastPos.Value != pos)
+            {
+                var trace = new TraceEvent
+                {
+                    X = pos.X,
+                    Y = pos.Y
+                };
+                var traceID = now.ToString();
+                //var docRef = db.Document("boards/abc123/players/xavier/traces/" + traceID);
+
+                // TODO: Presumably firestore keeps retrying on network failure? Check this.
+                //docRef.SetAsync(trace);
+                var path = $"/track?board_id=abc123&player_id=xavier&x={pos.X}&y={pos.Y}&timestamp={string.Format("{0:0.}", now)}";
+                Console.WriteLine(path);
+                client.GetAsync(path);
+            }
+            lastPos = pos;
+        }
+    }
+}
---
+++ client/OriTracker/OriTracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OriTracker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OriTracker")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3619d50f-9c38-43e7-9f70-503acdf96f7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
