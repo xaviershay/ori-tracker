52ec029a5f40ebbeb15c270362a20a680fb4bb16
Xavier Shay <contact@xaviershay.com>
2018-03-21 10:30:05 -0700

    WIP add react

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,13 +1,81 @@
-var Viewport = require('pixi-viewport')
-var PIXI = require('pixi.js')
-var firebase = require("firebase")
-var firestore = require("firebase/firestore")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase'
+import firestore from '@firebase/firestore'
 
 firebase.initializeApp({
   apiKey: 'AIzaSyBPza7LIiAnw0XZcoh9qJTjDXmI9q2El2U',
   authDomain: 'ori-tracker.firebaseapp.com',
   projectId: 'ori-tracker'
 });
+var db = firebase.firestore();
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { traces: [] }
+  }
+
+  componentWillMount() {
+    var me = this;
+    db
+      .collection('boards/abc123/players/xavier/traces')
+      .onSnapshot(function(snapshot) {
+        var maxContinuous = 50; // TODO: Figure out what makes sense here
+        var traces = []
+        var lastPos = null;
+        snapshot.forEach(function(doc) {
+          var data = doc.data();
+          /*
+          var x = toMapCoord(data)
+          if (lastPos == null || data.start) {
+            trace.moveTo(x.x, x.y)
+          } else {
+            if (distance(lastPos, x) > maxContinuous) {
+              trace.lineStyle(2, 0x00ff00, 0.5);
+            } else {
+              trace.lineStyle(2, 0xff0000, 0.8);
+            }
+            trace.lineTo(x.x, x.y)
+          }
+          */
+          data.id = doc.id;
+          traces.push(data);
+        })
+        console.log(traces);
+        me.setState({traces: traces})
+      })
+  }
+  render() {
+    return (
+      <div className="game">
+        <h1>Hello</h1>
+      <ul>
+        {
+          this.state.traces.map( k => <li key={k.id}>{k.x},{k.y}</li> )
+        }
+      </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
+registerServiceWorker();
+
+/*
+var Viewport = require('pixi-viewport')
+var PIXI = require('pixi.js')
+var firebase = require("firebase")
+var firestore = require("firebase/firestore")
+
+
+
 
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
@@ -39,7 +107,7 @@ function resize() {
 }
 
 resize()
-window.onresize = resize
+//window.onresize = resize
 
 
 document.body.appendChild(app.view);
@@ -135,3 +203,4 @@ function setup() {
       })
     })
 }
+*/
